323500641f269d46d66ccca633f696876de433d9
Gary Behan <gary.b@browserstack.com>
2025-11-27 11:59:33 +0000

    Initial Commit

--- src/test/java/org/browserstack/training/tests/LoginTests.java
+++ src/test/java/org/browserstack/training/tests/LoginTests.java
@@ -5,7 +5,6 @@ import org.browserstack.training.pageobjects.Login;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Reporter;
 import org.testng.annotations.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -56,5 +55,42 @@ public class LoginTests {
 
     }
 
+    @Test
+    public void failedLoginTestUsername() {
+        try {
+            homePage.goToLoginPage();
 
+            loginPage.performLogin(wrongUsername, correctPassword);
+
+            Assert.assertEquals(loginPage.getErrorElement().getText(), incorrectUsernameMessage);
+        } catch (Exception e) {
+            System.out.println("There was an error: " + e);
+        }
+    }
+
+    @Test
+    public void failedLoginTestPassword() {
+        try {
+            homePage.goToLoginPage();
+
+            loginPage.performLogin(correctUsername, wrongPassword);
+
+            Assert.assertEquals(loginPage.getErrorElement().getText(), incorrectPasswordMessage);
+        } catch (Exception e) {
+            System.out.println("There was an error: " + e);
+        }
+    }
+
+    @Test
+    public void failedLoginTestUsernameAndPassword() {
+        try {
+            homePage.goToLoginPage();
+
+            loginPage.performLogin(wrongUsername, wrongPassword);
+
+            Assert.assertEquals(loginPage.getErrorElement().getText(), incorrectUserAndPassMessage);
+        } catch (Exception e) {
+            System.out.println("There was an error: " + e);
+        }
+    }
 }
